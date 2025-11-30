1b6c53a19ed405232a98f97aa15523770512cf9a
Drake2034 <kaissous.hf@gmail.com>
2025-12-01 00:00:26 +0100

    forgot seed, oops

--- test.c
+++ test.c
@@ -4,8 +4,12 @@
 #include <time.h>
 
 int main(){
+    srand((unsigned int)time(NULL));
+    
     list_t* list = initList();
     for(int i; i < 10; i++){
+        //error: passing argument 2 of 'listPushFront' makes pointer from integer without a cast
+        //expected 'void *' but argument is of type 'int'
         int random = rand() % 10;
         listPushFront(list, random);
     }
